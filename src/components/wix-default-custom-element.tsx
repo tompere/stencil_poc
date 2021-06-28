2b9bb3c991e8a23849449d39337ae05bfec72860
Tom Peres <tomp@wix.com>
2021-06-28 17:54:47 +0300

    with property

--- src/components/wix-default-custom-element.tsx
+++ src/components/wix-default-custom-element.tsx
@@ -1,14 +1,16 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'wix-default-custom-element',
 })
 export class AppRoot {
 
+  @Prop() wham: string;
+
   render() {
     return (
       <div>
-        <h1>Hello Wix</h1>
+        <h1>{`wham ${this.wham}`}</h1>
       </div>
     );
   }
